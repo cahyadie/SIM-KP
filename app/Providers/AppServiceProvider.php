9474d12fc8ee68f995e229725393bb4815482e3c
cahyadie <cahyadiilham12@gmail.com>
2026-06-22 02:51:53 +0700

    railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Pagination\Paginator;
 use SocialiteProviders\Manager\SocialiteWasCalled;
 use SocialiteProviders\Azure\AzureExtendSocialite;
-
+use Illuminate\Support\Facades\URL; // <-- 1. Tambahkan import URL di sini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
         Event::listen(SocialiteWasCalled::class, AzureExtendSocialite::class);
+
+        // <-- 2. Tambahkan pengecekan HTTPS di sini -->
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
