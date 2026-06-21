ba11de835c06d5d37ddbad66eef08bbc376fdec2
cahyadie <cahyadiilham12@gmail.com>
2026-06-22 01:35:01 +0700

    compiled v1

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
 
-// Meneruskan request dari Vercel ke index.php bawaan Laravel
+// Memaksa Laravel menggunakan folder /tmp untuk semua file cache dan kompilasi di Vercel
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+
+// Meneruskan request ke index.php bawaan Laravel
 require __DIR__ . '/../public/index.php';
